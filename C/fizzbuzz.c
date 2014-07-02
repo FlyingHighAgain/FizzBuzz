d149013c94175416e01f7527496c08819279846f
FlyingHighAgain <tdnr.sym@gmail.com>
2014-07-03 00:45:25 +0900

    First Commit

---
+++ C/fizzbuzz.c
@@ -0,0 +1,23 @@
+/* fizzbuzz.c */
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			printf("%s\n", "FizzBuzz");
+		} else if (i % 3 == 0) {
+			printf("%s\n", "Fizz");
+		} else if (i % 5 == 0) {
+			printf("%s\n", "Buzz");
+		} else {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
+
+
