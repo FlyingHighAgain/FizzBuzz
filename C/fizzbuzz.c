42c7ca7718a67cd5dcaeda55b14c717a3c7a5de1
FlyingHighAgain <tdnr.sym@gmail.com>
2014-07-03 01:06:31 +0900

    format indents

--- C/fizzbuzz.c
+++ C/fizzbuzz.c
@@ -4,20 +4,20 @@
 
 int main()
 {
-	int i;
-	for (i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 == 0) {
-			printf("%s\n", "FizzBuzz");
-		} else if (i % 3 == 0) {
-			printf("%s\n", "Fizz");
-		} else if (i % 5 == 0) {
-			printf("%s\n", "Buzz");
-		} else {
-			printf("%d\n", i);
-		}
-	}
+  int i;
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      printf("%s\n", "FizzBuzz");
+    } else if (i % 3 == 0) {
+      printf("%s\n", "Fizz");
+    } else if (i % 5 == 0) {
+      printf("%s\n", "Buzz");
+    } else {
+      printf("%d\n", i);
+    }
+  }
 
-	return 0;
+  return 0;
 }
 
 
--- Haskell/fizzbuzz.hs
+++ Haskell/fizzbuzz.hs
@@ -1,10 +1,12 @@
 -- fizzbuzz.hs
 
-fizzbuzz n = map (\x -> 
-                    if       x `mod` 15 == 0 then "FizzBuzz"
-                    else if x `mod`  3 == 0 then "Fizz"
-                    else if x `mod`  5 == 0 then "Buzz"
-                    else                          (show x)) [1..n]
+fizzbuzz n = 
+  map (\x -> 
+    if       x `mod` 15 == 0 then "FizzBuzz"
+    else if x `mod`  3 == 0 then "Fizz"
+    else if x `mod`  5 == 0 then "Buzz"
+    else                          (show x))
+  [1..n]
                      
 main=print $ fizzbuzz 100
 
