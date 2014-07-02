90390f73266ee0dc35c560fac5cc16f9f9179074
FlyingHighAgain <tdnr.sym@gmail.com>
2014-07-03 01:14:21 +0900

    third commit

--- Haskell/fizzbuzz.hs
+++ Haskell/fizzbuzz.hs
@@ -2,9 +2,9 @@
 
 fizzbuzz n = 
   map (\x -> 
-    if       x `mod` 15 == 0 then "FizzBuzz"
-    else if x `mod`  3 == 0 then "Fizz"
-    else if x `mod`  5 == 0 then "Buzz"
+    if       x `mod` 3 == 0 && x `mod` 5 == 0 then "FizzBuzz"
+    else if x `mod` 3 == 0 then "Fizz"
+    else if x `mod` 5 == 0 then "Buzz"
     else                          (show x))
   [1..n]
                      
